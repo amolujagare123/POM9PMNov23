68f76e558bd1b820443e2dae28ac3fef48b0db76
amolu <amolujagare@gmail.com>
2023-10-31 21:23:32 +0530

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import pages.Login;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost/ip");
+
+        Login login = new Login(driver);
+
+        login.setTxtUsername("amolujagare@gmail.com");
+        login.setTxtPassword("admin123");
+        login.clickBtnLogin();
+    }
+
+    @Test
+    public void loginTest2()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost/ip");
+
+        Login login = new Login(driver);
+
+        login.setTxtUsername("ddsds@gmail.com");
+        login.setTxtPassword("dsdsd");
+        login.clickBtnLogin();
+    }
+}
